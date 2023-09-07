f2582e4d29cf1a4441fd6be2c5bb9e2bfe1aaf9d
Supreeti Kushwaha <sksupreetk11@gmail.com>
2023-09-07 16:58:25 +0530

    add choices to main.rb

--- app.rb
+++ app.rb
@@ -99,44 +99,4 @@ class App
       puts "Date: #{rental.date}, Book: #{rental.book.title}" if rental.person.id == input_person_id
     end
   end
-
-  def choices
-    puts 'Please Choose an Option by entering a number:'
-    puts '1. List all books.'
-    puts '2. List all people.'
-    puts '3. Create a person.'
-    puts '4. Create a book.'
-    puts '5. Create a rental.'
-    puts '6. List all rentals for a given person id.'
-    puts '7. EXIT'
-  end
-
-  def choose_any(choice)
-    case choice
-    when 1
-      list_books
-    when 2
-      list_persons
-    when 3
-      create_person
-    when 4
-      create_book
-    when 5
-      create_rental
-    when 6
-      list_rentals
-    end
-  end
-
-  def show
-    loop do
-      choices
-      choice = gets.chomp.to_i
-      if choice >= 7
-        puts 'Thank you for using this app'
-        break
-      end
-      choose_any(choice)
-    end
-  end
 end
--- main.rb
+++ main.rb
@@ -1,7 +1,40 @@
 require_relative 'app'
+app = App.new
 
-def main
-  App.run
+def choices(app)
+  puts 'Welcome to School Library App!'
+  loop do
+    puts 'Please choose an option by enterin a number:'
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person id'
+    puts '7 - Exit'
+
+    num = gets.chomp.to_i
+    break if num == 7
+
+    choose_any(app, num)
+  end
 end
 
-main
+def choose_any(app, num)
+  case num
+  when 1
+    app.list_books
+  when 2
+    app.list_people
+  when 3
+    app.create_person
+  when 4
+    app.create_book
+  when 5
+    app.create_rental
+  when 6
+    app.list_rentals
+  end
+end
+
+choices(app)
